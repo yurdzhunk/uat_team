89e1cd90cb507b5cf712a1dfdc789a3bcadb8642
yurdzhunk <yurii.dzhunkovskyi@Yurii.Dzhunkovskyi>
2023-05-12 11:00:51 +0300

    first commit

---
+++ my-app/app/page.js
@@ -0,0 +1,11 @@
+import Image from 'next/image'
+import styles from './page.module.css'
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <h1>Hello UAT</h1>
+      <h1>This is master branch</h1>
+    </main>
+  )
+}
